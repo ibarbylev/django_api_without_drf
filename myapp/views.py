961ce98f04cd49c75d889468178b8d263e4495ae
su <ibarbylev@gmail.com>
2021-12-26 12:03:51 +0200

    first result

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+
+
+def my_api_view(request):
+    data = {
+        'name': request.user.username,  # username of current logged-in user, otherwise Anonymous
+        'url': 'https://www.pyscoop.com/',
+        'skills': ['Python', 'Django'],
+    }
+    return JsonResponse(data)
+
+
+class MyAPIView(View):
+    def get(self, request):
+        data = {
+            'name': request.user.username,  # username of current logged-in user, otherwise Anonymous
+            'url': 'https://www.pyscoop.com/',
+            'skills': ['Python', 'Django'],
+        }
+
+        return JsonResponse(data)
