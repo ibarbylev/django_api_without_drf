5afdcbf066174acbe8d9d7bf85fc9e7bf0fb9355
su <ibarbylev@gmail.com>
2021-12-27 00:10:45 +0200

    first result +

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import my_api_view
+from .views import my_api_view, MyAPIView
 
 urlpatterns = [
     path('awesome-api/', my_api_view),
+    path('awesome-api-view-as-class/', MyAPIView.as_view()),
 ]
